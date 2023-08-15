7f40e54fa01158d1c3b41a0c9a7264d019ad257f
Tyler Tarsi <hi@ttarsi.com>
2023-08-15 14:20:07 -0400

    fix: var reference

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ module "obs_staging_vpc" {
     config       = "staging"
   }
   blockscout_settings = {
-    blockscout_docker_image = var.blockscout_dev_docker_image
+    blockscout_docker_image = var.blockscout_docker_image
     rpc_address             = "http://staging.omni.network:8545"
     ws_address              = "ws://staging.omni.network:8546"
     chain_id                = "165"
@@ -59,7 +59,7 @@ module "obs_testnet_vpc" {
     config       = "testnet"
   }
   blockscout_settings = {
-    blockscout_docker_image = var.blockscout_dev_docker_image
+    blockscout_docker_image = var.blockscout_docker_image
     rpc_address             = "http://testnet-sentry-explorer.omni.network:8545"
     ws_address              = "ws://testnet-sentry-explorer.omni.network:8546"
     chain_id                = "165"
